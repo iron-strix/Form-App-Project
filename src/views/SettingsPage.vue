39c7673bf0ebcb91ab9acb4bf6432cba9cc43efd
iron-strix <nigel.strix.dev@gmail.com>
2022-11-24 16:27:04 -0600

    added css for multiple checkbox components

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -2,7 +2,8 @@
 import CheckBox from '@/components/CheckBox.vue'
 import { ref } from 'vue'
 
-const darkMode = ref(true)
+const darkMode = ref(false)
+const colorBlindMode = ref(false)
 </script>
 
 <template>
@@ -12,6 +13,9 @@ const darkMode = ref(true)
       <div class="settings-form">
         <div class="settings-form-item">
           <check-box v-model:checked="darkMode" field-id="dark" label="Dark Mode" />
+        </div>
+        <div class="settings-form-item">
+          <check-box v-model:checked="colorBlindMode" field-id="colorblind" label="Color Blind Mode" />
         </div>
       </div>
     </div>
@@ -24,5 +28,15 @@ const darkMode = ref(true)
 }
 .header {
   @apply mx-auto w-full p-8 text-start align-top text-4xl text-slate-800;
+}
+
+.settings-form {
+  @apply ml-8;
+  &-item {
+    @apply items-center justify-center;
+    & label {
+      @apply flex;
+    }
+  }
 }
 </style>
